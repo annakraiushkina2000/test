6cfce6b81ae4cf4cf03dcab23b1e2f3f5a3c8ec4
Anna <Kraiushkinih.dom@yandex.ru>
2021-09-12 11:00:56 +0300

    feature-2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ class HelloWorld {
         System.out.println("B");
 =======
 >>>>>>> 71b8ed256ede8d36b21b16e2fc7a679056a310b7
-        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
     }
 }
